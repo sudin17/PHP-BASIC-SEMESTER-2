06bb16cd05cb849717b5fe05fc06d580ccf06add
sudin17 <lgfc8080@gmail.com>
2023-04-02 05:56:02 +0800

    basic php

---
+++ latihan2.php
@@ -0,0 +1,23 @@
+<?php 
+    // membuat variabel
+    $nama = "sudin";
+    $nim =  "2201010029";
+    $umur = 19;
+    $ip1 = 4.00;
+    $ip2   = 3.90;
+
+    // operator aritmatika
+    $ipk = ($ip1 + $ip2) / 2;
+
+    // menampilkan isi variabel
+    echo "DATA MAHASISWA <hr>";
+    echo "Nama = $nama <br>";
+    echo "Nim  = $nim <br>";
+    echo "Umur = $umur <br>";
+    echo "ip1  = $ip1 <br>";
+    echo "ip2  = $ip2 <br>";
+    echo "ipk = $ipk <br>";
+
+    
+
+?>
---
+++ latihan3.php
@@ -0,0 +1,32 @@
+<?php 
+    // diketahui
+    $sisi = 8;
+
+
+    // rumus luas
+    $luas = $sisi * $sisi;
+
+    // rumus keliling
+    $keliling = 4 * $sisi;
+
+    echo "LUAS DAN KELILING PERSEGI ADALAH SEBAGAI BERIKUT <hr>";
+    echo "diketahui sisi persegi = $sisi <br>";
+    echo "luas persegi adalah = $luas <br>";
+    echo "keliling persegi adalah = $keliling <br> <br>";
+
+    // diketahui
+    $panjang = 17;
+    $lebar = 9;
+
+    // rumus luas
+    $luas1 = $panjang * $lebar;
+
+    // rumus keliling
+    $keliling1 = (2*$panjang) + (2*$lebar);
+
+    echo "LUAS DAN KELILING PERSEGI PANJANG ADALAH SEBAGAI BERIKUT <hr>";
+    echo "diketahui panjang persegi panjang= $panjang <br>";
+    echo "diketahui lebar persegi panjang= $lebar <br>";
+    echo "luas persegi panjang adalah = $luas1 <br>";
+    echo "keliling persegi panjang adalah = $keliling1 <br>";
+?>
